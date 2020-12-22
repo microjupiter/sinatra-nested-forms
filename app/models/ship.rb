4909d53471a0ad2e963eaa09664f87fc5a53953f
Kyle Wilson <kyle@hinterlandstills.com>
2020-12-22 14:55:51 -0600

    commit tree c75197e379890d182c597d6935a862eed7d15513

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
-end
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(name, weight, height)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+  end
